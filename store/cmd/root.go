a501a9e8d2ffaf8185073926aeb378d3d03f15ee
Rusty Eddy <rusty@rustyeddy.com>
2019-07-02 14:03:31 -0700

    initial commit

--- store/cmd/root.go
+++ store/cmd/root.go
@@ -31,7 +31,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "foo",
+  Use:   "store",
   Short: "A brief description of your application",
   Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -60,7 +60,7 @@ func init() {
   // Cobra supports persistent flags, which, if defined here,
   // will be global for your application.
 
-  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.foo.yaml)")
+  rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.store.yaml)")
 
 
   // Cobra also supports local flags, which will only run
@@ -82,9 +82,9 @@ func initConfig() {
       os.Exit(1)
     }
 
-    // Search config in home directory with name ".foo" (without extension).
+    // Search config in home directory with name ".store" (without extension).
     viper.AddConfigPath(home)
-    viper.SetConfigName(".foo")
+    viper.SetConfigName(".store")
   }
 
   viper.AutomaticEnv() // read in environment variables that match
--- store/main.go
+++ store/main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package main
 
-import "foo/cmd"
+import "github.com/rustyeddy/store/cmd"
 
 func main() {
   cmd.Execute()
